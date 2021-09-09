b327ce3940c1451f533a02046f30def1819c1868
ptkkhiem <ptkkhiem@gmail.com>
2021-09-09 19:59:33 +0700

    Update script.js

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,13 @@ function binaryToDecimal() {
 	var binary = document.getElementById("binary").value;
 	
 	if(binary == ""){
-		alert("Please fill up the required field!");
+		alert("Vui lòng nhập số trường bắt buộc!");
 	}else{
 		if(/^[01]+$/.test(binary)){
 			var decimal = parseInt(binary, 2);
 			document.getElementById("result").innerHTML = "<center><label style='font-size:18px;'>SỐ THẬP PHÂN TƯƠNG ỨNG LÀ</label></center><center><h3 class='text-primary'>"+decimal+"</h3></center>";
 		}else{
-			alert("Please enter binary numbers");
+			alert("Vui lòng nhập đúng số NHỊ PHÂN");
 		}
 	}
 }
